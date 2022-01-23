e2974fd59c3828c7c0f4811b13d1cb9793929267
Andy <andy.cao@roamdigital.com>
2022-01-23 15:48:23 +1300

    chore: rename the name of the proj

--- ios/ReactNativeMacOSDemoTests/ReactNativeMacOSDemoTests.m
+++ ios/ReactNativeMacOSDemoTests/ReactNativeMacOSDemoTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface rnmacosapptestTests : XCTestCase
+@interface ReactNativeMacOSDemoTests : XCTestCase
 
 @end
 
-@implementation rnmacosapptestTests
+@implementation ReactNativeMacOSDemoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
