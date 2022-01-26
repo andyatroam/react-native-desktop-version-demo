04e6fd0241a834f018eb6c4146098198d9ce8db7
Andy <andy.cao@roamdigital.com>
2022-01-27 11:14:03 +1300

    feat: update navigation add fonts

--- src/navigation/WelcomeStack.tsx
+++ src/navigation/WelcomeStack.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import WelcomeScreen from '../examples/welcome/WelcomeScreen';
+import WelcomeScreen from '../examples/welcome/SplashScreen';
 
 const Stack = createStackNavigator();
 
